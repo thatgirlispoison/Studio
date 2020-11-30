5f85714fdc30dabca1e1738d30d3d734de0cbb5f
Kelly Thomas <kellydye77@gmail.com>
2020-11-30 13:37:14 -0600

    Studio still needs last bonus mission

---
+++ Studio/Circle.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Studio
+{
+    public class Circle
+    {
+        public static double AskRadius()
+        {
+            Console.WriteLine("What is the radius of the circle?");
+            string radius = Console.ReadLine();
+            double dubRadius = double.Parse(radius);
+            while (dubRadius <= 0)
+            {
+                Console.WriteLine("Please only enter a positive number.");
+                Console.WriteLine("What is the radius of the circle?");
+                radius = Console.ReadLine();
+                dubRadius = double.Parse(radius);
+            }
+            double area = Math.Pow(dubRadius, dubRadius) * Math.PI;
+            return area;
+
+
+        }
+    }
+}
---
+++ Studio/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Studio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double getArea = Circle.AskRadius();
+            Console.WriteLine("How many miles per gallon do you get for your car?");
+            string mpg = Console.ReadLine();
+            double dubMpg = double.Parse(mpg);
+            double gallons = getArea / dubMpg;
+            Console.WriteLine($"You would use {gallons} gallons of gas for the trip.");
+
+
+
+
+        }
+    }
+}
